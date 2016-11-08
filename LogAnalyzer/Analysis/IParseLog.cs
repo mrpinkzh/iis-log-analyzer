fbb4ec42b9dd367d69328417e0e96ea7d6aed661
Manuel Markwalder <manuel.markwalder@aias.ch>
2016-11-08 20:45:54 +0100

    cleaned up the build

---
+++ LogAnalyzer/Analysis/IParseLog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LogAnalyzer.Analysis
+{
+    /// <summary>
+    /// An implementer of <see cref="IParseLog"/> is capable of parsing lines
+    /// with log information to <see cref="LogItem"/>s.
+    /// </summary>
+    public interface IParseLog
+    {
+        /// <summary>
+        /// Parses the information in the specified <paramref name="lines"/> and returns it
+        /// in form of  <see cref="LogItem"/>s.
+        /// </summary>
+        /// <param name="lines">A collection of strings containing log information.</param>
+        /// <returns>A read only list of strings.</returns>
+        IReadOnlyList<LogItem> Parse(IReadOnlyCollection<string> lines);
+    }
+}
---
+++ LogAnalyzer/Analysis/W3cExtendedLogFileFormatParser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LogAnalyzer.Analysis
+{
+    /// <summary>
+    /// The <see cref="W3cExtendedLogFileFormatParser"/> is capable of parsing lines
+    /// with log information in the extended log file format defined by the W3c.
+    /// https://www.w3.org/TR/WD-logfile.html
+    /// </summary>
+    public class W3cExtendedLogFileFormatParser : IParseLog
+    {
+        /// <summary>
+        /// Parses the information in the specified <paramref name="lines"/> and returns it
+        /// in form of  <see cref="LogItem"/>s.
+        /// </summary>
+        /// <param name="lines">A collection of strings containing log information.</param>
+        /// <returns>A read only list of strings.</returns>
+        public IReadOnlyList<LogItem> Parse(IReadOnlyCollection<string> lines)
+        {
+            return null;
+        }
+    }
+}
